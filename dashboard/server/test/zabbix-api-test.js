e933ed0646cc4ae9a063203b24035f47f720c806
schengcrouchingtiger <scheng@contactpointsolutions.com>
2015-03-06 11:26:39 -0700

    timeout with async and delay

--- dashboard/server/test/zabbix-api-test.js
+++ dashboard/server/test/zabbix-api-test.js
@@ -2,8 +2,14 @@ var ZabbixApi = require( 'zabbix-api' );
 var expect = require( 'chai' ).expect,
     should = require( 'chai' ).should();
 
+var _ = require( 'lodash' );
+
+var async = require( 'async' );
+
 
 describe( 'ZabbixApi', function() {
+    this.timeout( 5000 );
+
     var api_url = 'http://zabbix.cpscloud.com/zabbix/api_jsonrpc.php';
     var client = new ZabbixApi( 'reports', 'yI9SJmVkB5SdM', api_url );
     var fakeone = new ZabbixApi( 'fake', 'fake', api_url );
@@ -32,24 +38,56 @@ describe( 'ZabbixApi', function() {
     describe( '#request', function() {
         var hostid = '10300';
         it( 'should return an array of object containing property `hostid`', function(done) {
-            client.request( 'host.get', {
-                hostids: hostid
-            }, function(err, result) {
-                    should.not.exist( err );
-                    result.should.be.instanceof( Array ).and.lengthOf( 1 );
-                    result[0].should.have.property( 'hostid' ).with.equal( hostid );
-                    done();
-                } );
+
+            var run = function() {
+                client.request( 'host.get', {
+                    hostids: hostid
+                }, function(err, result) {
+                        should.not.exist( err );
+                        result.should.be.instanceof( Array ).and.lengthOf( 1 );
+                        result[0].should.have.property( 'hostid' ).with.equal( hostid );
+                        done();
+                    } );
+            };
+
+            _.delay( run, 1000 );
+
+
+        } );
+
+        it( 'should retry successfully and return an array of object containing property `hostid`', function(done) {
+            var task = function(callback, results) {
+                client.request( 'host.get', {
+                    hostids: hostid
+                }, function(err, result) {
+                        callback( err, result );
+                    } );
+            };
+
+            var callback = function(err, result) {
+
+                should.not.exist( err );
+                result.should.be.instanceof( Array ).and.lengthOf( 1 );
+                result[0].should.have.property( 'hostid' ).with.equal( hostid );
+                done();
+            };
+
+            async.retry( 5, task, callback );
         } );
 
         it( 'should reutrn error when authentication failed', function(done) {
-            fakeone.request( 'host.get', {
-                hostids: hostid
-            }, function(err, result) {
-                    should.not.exist( result );
-                    err.should.be.instanceof( Object );
-                    done();
-                } );
+            var run = function() {
+                fakeone.request( 'host.get', {
+                    hostids: hostid
+                }, function(err, result) {
+                        should.not.exist( result );
+                        err.should.be.instanceof( Object );
+                        done();
+                    } );
+            };
+
+            _.delay( run, 2000 );
+
         } );
     } );
 
