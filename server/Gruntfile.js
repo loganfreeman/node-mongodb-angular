a0f7bb57a74b451da64dda021208f7bed29bf841
schengcrouchingtiger <scheng@contactpointsolutions.com>
2015-03-17 12:41:19 -0600

    grunt test task

--- server/Gruntfile.js
+++ server/Gruntfile.js
@@ -1,1 +1,23 @@
-module.exports = function(grunt) {};
+module.exports = function(grunt) {
+
+    require( 'load-grunt-tasks' )( grunt );
+
+
+    grunt.initConfig( {
+        pkg: grunt.file.readJSON( 'package.json' ),
+        // Configure a mochaTest task
+        mochaTest: {
+            test: {
+                options: {
+                    reporter: 'spec',
+                    captureFile: 'results.txt', // Optionally capture the reporter output to a file
+                    quiet: false, // Optionally suppress output to standard out (defaults to false)
+                    clearRequireCache: false // Optionally clear the require cache before running tests (defaults to false)
+                },
+                src: ['test/**/*.js', '!zabbix-api-test.js']
+            }
+        }
+    } );
+
+    grunt.registerTask( 'test', ['mochaTest'] );
+};
