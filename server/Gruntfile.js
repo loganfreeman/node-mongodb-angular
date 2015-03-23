c99e6facd539a56f30b631c32ac8d44a75b59241
schengcrouchingtiger <scheng@contactpointsolutions.com>
2015-03-23 14:46:17 -0600

    all

--- server/Gruntfile.js
+++ server/Gruntfile.js
@@ -14,7 +14,16 @@ module.exports = function(grunt) {
                     quiet: false, // Optionally suppress output to standard out (defaults to false)
                     clearRequireCache: false // Optionally clear the require cache before running tests (defaults to false)
                 },
-                src: ['test/**/*.js', '!zabbix-api-test.js']
+                src: ['test/**/*.js']
+            },
+            server: {
+                options: {
+                    reporter: 'spec',
+                    captureFile: 'results.txt', // Optionally capture the reporter output to a file
+                    quiet: false, // Optionally suppress output to standard out (defaults to false)
+                    clearRequireCache: false // Optionally clear the require cache before running tests (defaults to false)
+                },
+                src: ['rest/**/*.js']
             }
         },
 
@@ -34,5 +43,7 @@ module.exports = function(grunt) {
         }
     } );
 
-    grunt.registerTask( 'test', ['mochaTest'] );
+    grunt.registerTask( 'test', ['mochaTest:test'] );
+
+    grunt.registerTask( 'rest', ['mochaTest:server'] );
 };
---
+++ server/rest/user.js
@@ -0,0 +1,59 @@
+var Promise = require( 'bluebird' );
+var request = Promise.promisify( require( 'request' ) );
+// request.debug = true;
+
+var queryString = require( 'querystring' );
+
+var db = require( '../src/jugglingdb/init.js' );
+
+
+var _ = require( 'lodash' );
+
+var http = require( 'http' );
+
+var helpers = require( '../test/helpers.js' );
+
+var expect = require( 'chai' ).expect,
+    should = require( 'chai' ).should();
+
+var assert = require( 'assert' );
+
+describe( 'user route', function() {
+    it( 'should return 200', function(done) {
+        http.get( helpers.getUrl( '/' ), function(res) {
+            assert.equal( 200, res.statusCode );
+            console.log( res.body );
+            done();
+        } );
+    } );
+
+    it( 'it should login', function(done) {
+        var postData = {
+            username: 'scheng',
+            password: '@33yyy'
+        };
+
+        var body = queryString.stringify( postData );
+
+        var options = {
+            url: 'http://localhost:8081/login',
+            body: body,
+            method: 'POST',
+            headers: {
+                'content-type': 'application/x-www-form-urlencoded'
+            },
+        };
+
+        request( options )
+            .spread( function(res, body) {
+                console.log( body );
+                var user = JSON.parse( body );
+                expect( user.email ).to.be.eq( 'scheng@contactpointsolutions.com' );
+                done();
+            } )
+            .catch( function(err) {
+                console.log( err );
+                done( err );
+            } );
+    } );
+} );
