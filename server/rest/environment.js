eb69a8cf8dec665b876491e9a788b8693e87e14b
scheng <shanhong.cheng@remedyinformatics.com>
2015-03-25 16:25:37 -0600

    env test

--- server/rest/environment.js
+++ server/rest/environment.js
@@ -1,34 +1,49 @@
-var Promise = require( 'bluebird' );
-var request = Promise.promisify( require( 'request' ) );
+var Promise = require('bluebird');
+var request = Promise.promisify(require('request'));
 // request.debug = true;
 
-var queryString = require( 'querystring' );
+var queryString = require('querystring');
 
-var db = require( '../src/jugglingdb/init.js' );
-
-
-var _ = require( 'lodash' );
-
-var http = require( 'http' );
-
-var helpers = require( '../test/helpers.js' );
-
-var expect = require( 'chai' ).expect,
-    should = require( 'chai' ).should();
-
-var assert = require( 'assert' );
+var db = require('../src/jugglingdb/init.js');
 
 
-describe( 'environment routes', function() {
-    it( 'should return all environments', function(done) {
-        request( 'http://localhost:8081/environments' )
-            .spread( function(res, body) {
-                var environs = JSON.parse( body );
-                helpers.isJSONArray( environs );
+var _ = require('lodash');
+
+var http = require('http');
+
+var helpers = require('../test/helpers.js');
+
+var expect = require('chai').expect,
+    should = require('chai').should();
+
+var assert = require('assert');
+
+
+describe('environment routes', function() {
+    it('should return all environments', function(done) {
+        request('http://localhost:8081/environments')
+            .spread(function(res, body) {
+                var environs = JSON.parse(body);
+                helpers.isJSONArray(environs);
+                _.each(environs, function(env) {
+                    env.should.have.property('$stacks');
+                })
                 done();
-            } )
-            .catch( function(e) {
-                done( e );
-            } );
-    } );
-} );
+            })
+            .catch(function(e) {
+                done(e);
+            });
+    });
+
+    it('should get environment by ID', function(done) {
+        request('http://localhost:8081/environment/1')
+            .spread(function(res, body) {
+                var environment = JSON.parse(body);
+                environment.should.have.property('$stacks');
+                done();
+            })
+            .catch(function(err) {
+                done(err);
+            })
+    })
+});
--- server/src/routes/environment.js
+++ server/src/routes/environment.js
@@ -1,13 +1,23 @@
-var envController = require( '../controller/environmentController.js' );
+var envController = require('../controller/environmentController.js');
 
 module.exports = function(app) {
-    app.get( '/environments', function(req, res) {
-        envController.getEnvironments()
-            .then( function(models) {
-                res.json( models );
-            } )
-            .catch( function(e) {
-                res.status( 500 ).send( e );
-            } );
-    } );
+	app.get('/environments', function(req, res) {
+		envController.getEnvironments()
+			.then(function(models) {
+				res.json(models);
+			})
+			.catch(function(e) {
+				res.status(500).send(e);
+			});
+	});
+
+	app.get('/environment/:id', function(req, res) {
+		envController.getEnvironmentById(req.params.id)
+			.then(function(env) {
+				res.json(env);
+			})
+			.catch(function(err) {
+				res.status(500).send(err);
+			})
+	})
 };
