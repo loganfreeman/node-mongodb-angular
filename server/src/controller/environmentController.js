2de68885833fcc750e492b21f9a1623970a8084d
scheng <shanhong.cheng@remedyinformatics.com>
2015-03-25 16:15:36 -0600

    all

--- server/src/controller/environmentController.js
+++ server/src/controller/environmentController.js
@@ -1,36 +1,92 @@
-var schema = require( '../jugglingdb/init.js' );
+var schema = require('../jugglingdb/init.js');
 
-var Promise = require( 'bluebird' );
+var Promise = require('bluebird');
 
-var _ = require( 'lodash' );
+var _ = require('lodash');
 
 
 module.exports = {
 
 
     getEnvironments: function() {
-        return new Promise( function(resolve, reject) {
-                schema['environment'].all( function(err, models) {
-                    if (err) {
-                        reject( err );
-                    } else {
-                        resolve( models );
-                    }
-                } );
-            } );
+        return new Promise(function(resolve, reject) {
+            schema['environment'].all(function(err, models) {
+                if (err) {
+                    reject(err);
+                } else {
+                    Promise.resolve(models)
+                        .map(function(env) {
+                            return new Promise(function(resolve, reject) {
+                                env.stacks(function(err, stacks) {
+                                    if (err) {
+                                        reject(err);
+                                    } else {
+                                        env.$stacks = stacks;
+                                        resolve(env);
+                                    }
+                                })
+                            })
+                        })
+                        .then(function(promises) {
+                            Promise.all(promises)
+                                .then(function(envs) {
+                                    resolve(envs);
+                                })
+                        })
+                }
+            });
+        });
     },
 
 
     create: function(instance) {
-        return new Promise( function(resolve, reject) {
-                schema['environment'].create( instance, function(err, model) {
-                    if (err) {
-                        reject( err );
-                    } else {
-                        resolve( model );
-                    }
-                } );
-            } );
+        return new Promise(function(resolve, reject) {
+            schema['environment'].create(instance, function(err, model) {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(model);
+                }
+            });
+        });
+    },
+
+    getEnvironmentById: function(id) {
+        return new Promise(function(resolve, reject) {
+            schema['environment'].find(id, function(err, model) {
+                if (err) {
+                    reject(err);
+                } else {
+                    // resolve(model);
+                    model.stacks(function(err, stacks) {
+                        if (err) {
+                            reject(err);
+                        } else {
+                            model.$stacks = stacks;
+                            resolve(model);
+                        }
+
+                    })
+                }
+            });
+        });
+    },
+
+    delete: function(id) {
+        var self = this;
+        return new Promise(function(resolve, reject) {
+            self.getEnvironmentById(id)
+                .then(function(env) {
+                    //console.log('about to destroy ' + JSON.stringify(env));
+                    env.destroy(function(err, model) {
+                        if (err) {
+                            reject(err);
+                        } else {
+                            resolve(model);
+                        }
+                    })
+                })
+        })
     }
 
 };
--- server/test/controller/environmentController.js
+++ server/test/controller/environmentController.js
@@ -7,56 +7,89 @@
  */
 
 
-var controller = require( '../../src/controller/environmentController.js' );
+var controller = require('../../src/controller/environmentController.js');
 
-var db = require( '../../src/jugglingdb/init.js' );
-
-
-var _ = require( 'lodash' );
-
-var expect = require( 'chai' ).expect,
-    should = require( 'chai' ).should();
+var db = require('../../src/jugglingdb/init.js');
 
 
-var helpers = require( '../helpers.js' );
+var _ = require('lodash');
+
+var expect = require('chai').expect,
+    should = require('chai').should();
+
+
+var helpers = require('../helpers.js');
 
 
 
-describe( 'environmentController', function() {
-    it( 'should return environments', function(done) {
+describe('environmentController', function() {
+
+    it('should return environment by ID', function(done) {
+        controller.getEnvironmentById(1)
+            .then(function(env) {
+                // console.log(JSON.stringify(env));
+                env.should.have.property('$stacks');
+                helpers.verifyArray(env.$stacks, db['stack']);
+                done();
+            })
+            .catch(function(err) {
+                done(err);
+            })
+    });
+
+    it('should return environments', function(done) {
         controller.getEnvironments()
-            .then( function(models) {
+            .then(function(models) {
 
 
-                helpers.verifyArray( models, db['environment'] );
+                helpers.verifyArray(models, db['environment']);
                 // environment should have many stacks
-                _.each( models, function(model) {
-                    model.stacks( function(err, stacks) {
-                        helpers.verifyArray( stacks, db['stack'] );
-                    } );
-                } );
+                _.each(models, function(model) {
+                    /*model.stacks(function(err, stacks) {
+                        helpers.verifyArray(stacks, db['stack']);
+                    });*/
+                    model.should.have.property('$stacks');
+                    helpers.verifyArray(model.$stacks, db['stack']);
+                });
                 done();
             }, function(err) {
-                    done( err );
-                } );
-    } );
+                done(err);
+            });
+    });
 
-    it( 'should create new environment', function(done) {
+    it('should create new environment', function(done) {
         var data = {
             name: 'stage',
             description: 'stage environment'
         };
-        controller.create( data )
-            .then( function(model) {
-                model.should.be.instanceof( db['environment'] );
-                model.should.have.property( 'id' );
+        controller.create(data)
+            .then(function(model) {
+                model.should.be.instanceof(db['environment']);
+                model.should.have.property('id');
                 done();
             }, function(err) {
-                    if (helpers.keyExists( err )) {
-                        done();
-                    } else {
-                        done( err );
-                    }
-                } );
-    } );
-} );
+                if (helpers.keyExists(err)) {
+                    done();
+                } else {
+                    done(err);
+                }
+            });
+    });
+
+    it('should delete env', function(done) {
+        var data = {
+            name: 'temp',
+            description: 'temp environment'
+        };
+        controller.create(data)
+            .then(function(env) {
+                return controller.delete(env.id);
+            })
+            .then(function(env) {
+                done();
+            })
+            .catch(function(err) {
+                done(err);
+            })
+    })
+});
