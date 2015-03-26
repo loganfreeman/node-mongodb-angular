7f5e05590101e1e715334de5a0c61b90171ef371
schengcrouchingtiger <scheng@contactpointsolutions.com>
2015-03-26 15:45:42 -0600

    update deploy

--- server/rest/deploy.js
+++ server/rest/deploy.js
@@ -1,91 +1,116 @@
-var Promise = require('bluebird');
-var request = Promise.promisify(require('request'));
+var Promise = require( 'bluebird' );
+var request = Promise.promisify( require( 'request' ) );
 // request.debug = true;
 
-var queryString = require('querystring');
+var queryString = require( 'querystring' );
 
-var db = require('../src/jugglingdb/init.js');
-
-
-var _ = require('lodash');
-
-var http = require('http');
-
-var helpers = require('../test/helpers.js');
-
-var expect = require('chai').expect,
-	should = require('chai').should();
-
-var assert = require('assert');
-
-var uuid = require('node-uuid');
-
-describe('deploy routes', function() {
-	it('should get deploys', function(done) {
-		request('http://localhost:8081/deploys')
-			.spread(function(res, deploys) {
-				var deploys = JSON.parse(deploys);
-				_.each(deploys, function(instance) {
-					instance.should.have.property('instance_id');
-				})
-				done();
-			})
-			.catch(function(err) {
-				done(err);
-			})
-	});
-
-	it('should get deploys by instance ID', function(done) {
-		request('http://localhost:8081/deploys/instance/1')
-			.spread(function(res, deploys) {
-				var deploys = JSON.parse(deploys);
-				_.each(deploys, function(instance) {
-					instance.should.have.property('instance_id');
-				})
-				done();
-			})
-			.catch(function(err) {
-				done(err);
-			})
-	});
+var db = require( '../src/jugglingdb/init.js' );
 
 
-	it('should create then delete', function(done) {
-		var instance = {
-			deploy_date: new Date,
-			user_id: 2,
-			instance_id: 1,
-			comments: 'this is inserted by test'
-		};
-		var options = {
-			method: 'PUT',
-			url: 'http://localhost:8081/deploy',
-			json: instance
-		};
-		request(options)
-			.spread(function(res, body) {
-				if (res.statusCode != 200) {
-					throw Error(JSON.stringify(body));
-				};
-				return body;
-			})
-			.then(function(deploy) {
+var _ = require( 'lodash' );
 
-				// console.log('TO DELETE: ' + JSON.stringify(deploy));
-				var options = {
-					method: 'DELETE',
-					url: 'http://localhost:8081/deploy/' + deploy.id
-				};
-				request(options).
-				spread(function(res, body) {
-						done();
-					})
-					.catch(function(err) {
-						done(err);
-					})
-			})
-			.catch(function(err) {
-				done(err);
-			})
-	})
-})
+var http = require( 'http' );
+
+var helpers = require( '../test/helpers.js' );
+
+var expect = require( 'chai' ).expect,
+    should = require( 'chai' ).should();
+
+var assert = require( 'assert' );
+
+var uuid = require( 'node-uuid' );
+
+describe( 'deploy routes', function() {
+
+    it( 'should update deploy', function(done) {
+
+        var comments = 'this is updated through route ' + +new Date();
+        var data = {
+            comments: comments,
+            id: 2
+        };
+        var options = {
+            url: 'http://localhost:8081/deploy',
+            json: data,
+            method: 'POST'
+        };
+        request( options )
+            .spread( function(res, body) {
+                body.comments.should.be.eq( comments );
+                done();
+            } )
+            .catch( function(e) {
+                done( e );
+            } );
+
+    } );
+
+
+    it( 'should get deploys', function(done) {
+        request( 'http://localhost:8081/deploys' )
+            .spread( function(res, deploys) {
+                var deploys = JSON.parse( deploys );
+                _.each( deploys, function(instance) {
+                    instance.should.have.property( 'instance_id' );
+                } );
+                done();
+            } )
+            .catch( function(err) {
+                done( err );
+            } );
+    } );
+
+    it( 'should get deploys by instance ID', function(done) {
+        request( 'http://localhost:8081/deploys/instance/1' )
+            .spread( function(res, deploys) {
+                var deploys = JSON.parse( deploys );
+                _.each( deploys, function(instance) {
+                    instance.should.have.property( 'instance_id' );
+                } );
+                done();
+            } )
+            .catch( function(err) {
+                done( err );
+            } );
+    } );
+
+
+    it( 'should create then delete', function(done) {
+        var instance = {
+            deploy_date: new Date,
+            user_id: 2,
+            instance_id: 1,
+            comments: 'this is inserted by test'
+        };
+        var options = {
+            method: 'PUT',
+            url: 'http://localhost:8081/deploy',
+            json: instance
+        };
+        request(options)
+            .spread( function(res, body) {
+                if (res.statusCode != 200) {
+                    throw Error( JSON.stringify( body ) );
+                }
+                return body;
+            } )
+            .then( function(deploy) {
+
+                // console.log('TO DELETE: ' + JSON.stringify(deploy));
+                var options = {
+                    method: 'DELETE',
+                    url: 'http://localhost:8081/deploy/' + deploy.id
+                };
+                request( options ).
+                    spread( function(res, body) {
+                        done();
+                    } )
+                    .catch( function(err) {
+                        done( err );
+                    } );
+            } )
+            .catch( function(err) {
+                done( err );
+            } );
+    } );
+} );
