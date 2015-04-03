4ab0f71ffae3f66697c6c140ea5f5a89a315194c
schengcrouchingtiger <scheng@contactpointsolutions.com>
2015-04-02 18:07:32 -0600

    schema

---
+++ server/src/mongoose/models/drumset.js
@@ -0,0 +1,66 @@
+var mongoose = require( 'mongoose' ),
+    express = require( 'express' ),
+    assert = require( 'assert' ),
+    Promise = mongoose.Promise,
+    Schema = mongoose.Schema;
+
+
+/**
+ * Schema definition.
+ */
+
+var DrumsetSchema = new Schema( {
+    brand: String,
+    color: String,
+    type: String,
+    model: String
+} );
+
+/**
+ * Schema methods.
+ */
+
+DrumsetSchema.statics.useQuery = function() {
+    // return a Query
+    return this.find( {
+        color: 'black'
+    } ).sort( '_id' );
+};
+
+DrumsetSchema.statics.usePromise = function() {
+    var promise = new Promise();
+    this.find( {
+        type: 'Acoustic'
+    } )
+        .sort( '_id' )
+        .exec( promise.resolve.bind( promise ) );
+    return promise;
+};
+
+DrumsetSchema.statics.queryError = function() {
+    // should produce an invalid query error
+    return this.find( {
+        color: {
+            $fake: {
+                $boom: []
+            }
+        }
+    } ).sort( '_id' );
+};
+
+DrumsetSchema.statics.promiseError = function() {
+    var promise = new Promise;
+    promise.error( new Error( 'splat!' ) );
+    return promise;
+};
+
+DrumsetSchema.statics.usePromiseRedirect = function(status) {
+    var url = '/promise/redirect';
+    var promise = new Promise;
+    process.nextTick( function() {
+        promise.complete( url, status );
+    } );
+    return promise;
+};
+
+mongoose.model( 'Drumset', DrumsetSchema );
--- server/test/mongoose.js
+++ server/test/mongoose.js
@@ -8,71 +8,7 @@ var _ = require( 'lodash' );
 var expect = require( 'chai' ).expect,
     should = require( 'chai' ).should();
 
-const mongoose = require( 'mongoose' ),
-    express = require( 'express' ),
-    assert = require( 'assert' ),
-    Promise = mongoose.Promise,
-    Schema = mongoose.Schema;
-
-/**
- * Schema definition.
- */
-
-var DrumsetSchema = new Schema( {
-    brand: String,
-    color: String,
-    type: String,
-    model: String
-} );
-
-/**
- * Schema methods.
- */
-
-DrumsetSchema.statics.useQuery = function() {
-    // return a Query
-    return this.find( {
-        color: 'black'
-    } ).sort( '_id' );
-};
-
-DrumsetSchema.statics.usePromise = function() {
-    var promise = new Promise();
-    this.find( {
-        type: 'Acoustic'
-    } )
-        .sort( '_id' )
-        .exec( promise.resolve.bind( promise ) );
-    return promise;
-};
-
-DrumsetSchema.statics.queryError = function() {
-    // should produce an invalid query error
-    return this.find( {
-        color: {
-            $fake: {
-                $boom: []
-            }
-        }
-    } ).sort( '_id' );
-};
-
-DrumsetSchema.statics.promiseError = function() {
-    var promise = new Promise;
-    promise.error( new Error( 'splat!' ) );
-    return promise;
-};
-
-DrumsetSchema.statics.usePromiseRedirect = function(status) {
-    var url = '/promise/redirect';
-    var promise = new Promise;
-    process.nextTick( function() {
-        promise.complete( url, status );
-    } );
-    return promise;
-};
-
-mongoose.model( 'Drumset', DrumsetSchema );
+var mongoose = require( 'mongoose' );
 
 
 require( '../src/mongoose/models' );
@@ -97,7 +33,8 @@ describe( 'user schema', function() {
         firstname: 'barry',
         lastname: 'allan',
         email: 'barryallan@cctv.com',
-        password: 'hao123'
+        password: 'hao123',
+        username: 'barryallan'
     };
 
     before( function() {
@@ -109,15 +46,11 @@ describe( 'user schema', function() {
         db.close();
     } );
 
-    it( 'should set statics methods', function(done) {
+    it( 'should set statics methods', function() {
         UserCollection.should.have.property( 'load' );
+        UserCollection.should.have.property( 'create' );
 
-        UserCollection.load( {
-            email: 'barryallan@cctv.com'
-        } ).exec( function(err, user) {
-            expect( user ).to.be.null;
-            done();
-        } );
+
 
     } );
 } );
