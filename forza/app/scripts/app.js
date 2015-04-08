d7df51631dc8d82bbd705e4ff7c3298f97d759d0
schengcrouchingtiger <scheng@contactpointsolutions.com>
2015-04-08 15:48:28 -0600

    currentUser

--- forza/app/scripts/app.js
+++ forza/app/scripts/app.js
@@ -247,13 +247,6 @@ angular
     }] )
     .run( function($rootScope, $location, Auth) {
 
-        //watching the value of the currentUser variable.
-        $rootScope.$watch( 'currentUser', function(currentUser) {
-            // if no currentUser and on a page that requires authorization then try to update it
-            // will trigger 401s if user does not have a valid session
-            $rootScope.isLoggedIn = !currentUser;
-        } );
-
         Auth.currentUser();
 
         // On catching 401 errors, redirect to the login page.
