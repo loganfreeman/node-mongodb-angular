e0efd092ea4b268e0f4e3ee9fd6356ff6f32b1d0
scheng <shanhong.cheng@remedyinformatics.com>
2015-04-08 10:29:30 -0600

    all

--- forza/app/scripts/app.js
+++ forza/app/scripts/app.js
@@ -246,7 +246,7 @@ angular
             .otherwise({
                 redirectTo: '/'
             });
-        $locationProvider.html5Mode(true);
+        //$locationProvider.html5Mode(true);
     }])
     .run(function($rootScope, $location, Auth) {
 
