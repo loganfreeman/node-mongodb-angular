5c7a30f6b6d1b88543c688fdf8e397243f3e3110
schengcrouchingtiger <scheng@contactpointsolutions.com>
2015-04-09 15:49:14 -0600

    logo

--- forza/app/scripts/shared/Directives.js
+++ forza/app/scripts/shared/Directives.js
@@ -5,6 +5,18 @@ angular
     .constant( 'REGEXP', {
         INTEGER_REGEXP: /^\-?\d+$/
     } )
+    .controller( 'TimeCtrl', function TimeCtrl($scope, $timeout) {
+        $scope.clock = 'loading clock...'; // initialise the time variable
+        $scope.tickInterval = 1000; //ms
+
+        var tick = function() {
+            $scope.clock = Date.now(); // get the current time
+            $timeout( tick, $scope.tickInterval ); // reset the timer
+        };
+
+        // Start the timer
+        $timeout( tick, $scope.tickInterval );
+    } )
     .directive( 'disableAnimation', ['$animate', function($animate) {
             return {
                 restrict: 'A',
