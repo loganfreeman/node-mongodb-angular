0bff63f481e44960603593f09997e81085523142
schengcrouchingtiger <scheng@contactpointsolutions.com>
2015-04-09 16:59:05 -0600

    icons

--- forza/app/scripts/controllers/Navigation.js
+++ forza/app/scripts/controllers/Navigation.js
@@ -1,93 +1,107 @@
 'use strict';
 
 angular
-    .module('theme.navigation-controller', [])
-    .controller('NavigationController', ['$scope', '$location', '$timeout', '$global', function($scope, $location, $timeout, $global) {
-        $scope.menu = [
-
-        ];
-
-        var setParent = function(children, parent) {
-            angular.forEach(children, function(child) {
-                child.parent = parent;
-                if (child.children !== undefined) {
-                    setParent(child.children, child);
+    .module( 'theme.navigation-controller', [] )
+    .controller( 'NavigationController', ['$scope', '$location', '$timeout', '$global', function($scope, $location, $timeout, $global) {
+            $scope.menu = [
+                {
+                    label: 'Instances',
+                    iconClasses: 'fa fa-laptop',
+                    url: '#/'
+                },
+                {
+                    label: 'Stack',
+                    iconClasses: 'fa fa-cloud',
+                    url: '#/'
+                },
+                {
+                    label: 'User',
+                    iconClasses: 'fa fa-user',
+                    url: '#/'
                 }
-            });
-        };
+            ];
 
-        $scope.findItemByUrl = function(children, url) {
-            for (var i = 0, length = children.length; i < length; i++) {
-                if (children[i].url && children[i].url.replace('#', '') == url) return children[i];
-                if (children[i].children !== undefined) {
-                    var item = $scope.findItemByUrl(children[i].children, url);
-                    if (item) return item;
+            var setParent = function(children, parent) {
+                angular.forEach( children, function(child) {
+                    child.parent = parent;
+                    if (child.children !== undefined) {
+                        setParent( child.children, child );
+                    }
+                } );
+            };
+
+            $scope.findItemByUrl = function(children, url) {
+                for (var i = 0, length = children.length; i < length; i++) {
+                    if (children[i].url && children[i].url.replace( '#', '' ) == url) return children[i];
+                    if (children[i].children !== undefined) {
+                        var item = $scope.findItemByUrl( children[i].children, url );
+                        if (item) return item;
+                    }
                 }
-            }
-        };
+            };
 
-        setParent($scope.menu, null);
+            setParent( $scope.menu, null );
 
-        $scope.openItems = [];
-        $scope.selectedItems = [];
-        $scope.selectedFromNavMenu = false;
-
-        $scope.select = function(item) {
-            // close open nodes
-            if (item.open) {
-                item.open = false;
-                return;
-            }
-            for (var i = $scope.openItems.length - 1; i >= 0; i--) {
-                $scope.openItems[i].open = false;
-            }
             $scope.openItems = [];
-            var parentRef = item;
-            while (parentRef !== null) {
-                parentRef.open = true;
-                $scope.openItems.push(parentRef);
-                parentRef = parentRef.parent;
-            }
+            $scope.selectedItems = [];
+            $scope.selectedFromNavMenu = false;
 
-            // handle leaf nodes
-            if (!item.children || (item.children && item.children.length < 1)) {
-                $scope.selectedFromNavMenu = true;
-                for (var j = $scope.selectedItems.length - 1; j >= 0; j--) {
-                    $scope.selectedItems[j].selected = false;
+            $scope.select = function(item) {
+                // close open nodes
+                if (item.open) {
+                    item.open = false;
+                    return;
                 }
-                $scope.selectedItems = [];
+                for (var i = $scope.openItems.length - 1; i >= 0; i--) {
+                    $scope.openItems[i].open = false;
+                }
+                $scope.openItems = [];
                 var parentRef = item;
                 while (parentRef !== null) {
-                    parentRef.selected = true;
-                    $scope.selectedItems.push(parentRef);
+                    parentRef.open = true;
+                    $scope.openItems.push( parentRef );
                     parentRef = parentRef.parent;
                 }
-            }
-        };
 
-        $scope.$watch(function() {
-            return $location.path();
-        }, function(newVal, oldVal) {
-            if ($scope.selectedFromNavMenu == false) {
-                var item = $scope.findItemByUrl($scope.menu, newVal);
-                if (item) {
-                    $timeout(function() {
-                        $scope.select(item);
-                    });
+                // handle leaf nodes
+                if (!item.children || (item.children && item.children.length < 1)) {
+                    $scope.selectedFromNavMenu = true;
+                    for (var j = $scope.selectedItems.length - 1; j >= 0; j--) {
+                        $scope.selectedItems[j].selected = false;
+                    }
+                    $scope.selectedItems = [];
+                    var parentRef = item;
+                    while (parentRef !== null) {
+                        parentRef.selected = true;
+                        $scope.selectedItems.push( parentRef );
+                        parentRef = parentRef.parent;
+                    }
                 }
-            }
-            $scope.selectedFromNavMenu = false;
-        });
+            };
 
-        // searchbar
-        $scope.showSearchBar = function($e) {
-            $e.stopPropagation();
-            $global.set('showSearchCollapsed', true);
-        };
-        $scope.$on('globalStyles:changed:showSearchCollapsed', function(event, newVal) {
-            $scope.style_showSearchCollapsed = newVal;
-        });
-        $scope.goToSearch = function() {
-            $location.path('/extras-search');
-        };
-    }]);
+            $scope.$watch( function() {
+                return $location.path();
+            }, function(newVal, oldVal) {
+                    if ($scope.selectedFromNavMenu == false) {
+                        var item = $scope.findItemByUrl( $scope.menu, newVal );
+                        if (item) {
+                            $timeout( function() {
+                                $scope.select( item );
+                            } );
+                        }
+                    }
+                    $scope.selectedFromNavMenu = false;
+                } );
+
+            // searchbar
+            $scope.showSearchBar = function($e) {
+                $e.stopPropagation();
+                $global.set( 'showSearchCollapsed', true );
+            };
+            $scope.$on( 'globalStyles:changed:showSearchCollapsed', function(event, newVal) {
+                $scope.style_showSearchCollapsed = newVal;
+            } );
+            $scope.goToSearch = function() {
+                $location.path( '/extras-search' );
+            };
+    }] );
