3e2b4b76c4c98b8a5b140d51ce5c2d4774ba8fb9
schengcrouchingtiger <scheng@contactpointsolutions.com>
2015-04-10 09:44:29 -0600

    session test

--- forza/test/spec/controllers/session.js
+++ forza/test/spec/controllers/session.js
@@ -1,11 +1,27 @@
 describe( 'Session', function() {
 
+    var $httpBackend;
+    var $rootScope;
+
+    var Session;
+
     // First, we load the app's module
     beforeEach( module( 'angularPassportService' ) );
+    beforeEach( module( 'ngResource' ) );
+
+    beforeEach( inject( function($injector) {
+        $httpBackend = $injector.get( '$httpBackend' );
+        $rootScope = $injector.get( '$rootScope' );
+        Session = $injector.get( 'Session' );
+    } ) );
 
 
     it( 'should run', function() {
         console.log( 'test run' );
     } );
+
+    it( 'should define Session factory', inject( function() {
+        expect( Session ).toBeDefined();
+    } ) );
     // individual tests go here
 } );
