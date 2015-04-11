1488f20594d8e9df38a0485e146cb671cdcd87de
scheng <shanhong.cheng@remedyinformatics.com>
2015-04-10 21:09:51 -0600

    all

--- forza/app/scripts/pages/userManagementController.js
+++ forza/app/scripts/pages/userManagementController.js
@@ -1,11 +1,28 @@
 angular.module('theme.pages-controllers').controller('userManagementController',
 
-	['$q', '$location', '$scope', '$global', '$rootScope', 'Auth', function($q, $location, $scope, $global, $rootScope, Auth) {
+	['$q', '$location', '$scope', '$global', '$rootScope', 'Auth', '$http', function($q, $location, $scope, $global, $rootScope, Auth, $http) {
 		// TODO: 
 		console.log('In User Management Controller');
 		Auth.groups()
 			.then(function(groups) {
-				$scope.groups = groups.data;
+
+
+				var promises = [];
+				angular.forEach(groups.data, function(value, key) {
+					var group = value;
+					var promise = $q(function(resolve, reject) {
+						$http.get('/devops/group/{groupId}/users'.replace('{groupId}', group._id)).then(function(users) {
+							group.users = users.data;
+							resolve(group);
+						})
+					});
+					promises.push(promise);
+				});
+
+				$q.all(promises).then(function(groups) {
+					//console.log(groups);
+					$scope.groups = groups;
+				})
 			});
 
 		$scope.applyIconClass = function(obj) {
