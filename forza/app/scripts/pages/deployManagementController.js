4f0dff299644090e3abeb1f0fb104ac1fd1f281c
scheng <shanhong.cheng@remedyinformatics.com>
2015-04-13 18:49:54 -0600

    all

--- forza/app/scripts/pages/deployManagementController.js
+++ forza/app/scripts/pages/deployManagementController.js
@@ -1,38 +1,59 @@
-angular.module( 'theme.pages-controllers' ).controller( 'deployManagementController',
+angular.module('theme.pages-controllers').controller('deployManagementController',
 
     ['$q', '$location', '$scope', '$global', '$rootScope', 'Auth', '$http', 'applyIcon', function($q, $location, $scope, $global, $rootScope, Auth, $http, applyIcon) {
-            // TODO: 
-            Auth.deploys()
-                .then( function(deploys) {
+        // TODO: 
+        Auth.deploys()
+            .then(function(deploys) {
 
-                    $scope.deploys = deploys.data;
-                } );
+                $scope.deploys = deploys.data;
+            });
 
-            Auth.users()
-                .then( function(users) {
-                    $scope.users = users.data;
-                } );
-
-
-            Auth.instances()
-                .then( function(instances) {
-                    $scope.instances = instances.data;
-                } );
+        Auth.users()
+            .then(function(users) {
+                $scope.users = users.data;
+            });
 
 
-            $scope.deploy = {};
+        Auth.instances()
+            .then(function(instances) {
+                $scope.instances = instances.data;
+            });
+
+        $scope.openDateDialog = function($event) {
+            $event.preventDefault();
+            $event.stopPropagation();
+
+            $scope.dateDialogOpend = true;
+        };
+
+        $scope.dateOptions = {
+            formatYear: 'yy',
+            startingDay: 1
+        };
+
+        $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
+        $scope.format = $scope.formats[0];
+
+        $scope.today = function() {
+            $scope.deploy.deployDate = new Date();
+        };
 
 
 
-            $scope.applyIconClass = applyIcon;
+        $scope.deploy = {};
 
-            $scope.addDeploy = function(form) {
-                Auth.createDeploy( $scope.deploy )
-                    .then( function(deploy) {
-                        //alert( JSON.stringify( stack.data ) );
-                        $scope.deploys.push( deploy.data );
-                    } );
-            };
-        }
-    ]
+        $scope.today();
+
+
+
+        $scope.applyIconClass = applyIcon;
+
+        $scope.addDeploy = function(form) {
+            Auth.createDeploy($scope.deploy)
+                .then(function(deploy) {
+                    //alert( JSON.stringify( stack.data ) );
+                    $scope.deploys.push(deploy.data);
+                });
+        };
+    }]
 );
