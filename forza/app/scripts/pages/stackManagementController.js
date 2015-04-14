06e98fbb3954f02a75f966109707f72957aa4203
schengcrouchingtiger <scheng@contactpointsolutions.com>
2015-04-14 09:47:33 -0600

    all

--- forza/app/scripts/pages/stackManagementController.js
+++ forza/app/scripts/pages/stackManagementController.js
@@ -1,11 +1,20 @@
 angular.module( 'theme.pages-controllers' ).controller( 'stackManagementController',
 
-    ['$q', '$location', '$scope', '$global', '$rootScope', 'Auth', '$http', 'applyIcon', function($q, $location, $scope, $global, $rootScope, Auth, $http, applyIcon) {
+    ['$q', '$location', '$scope', '$global', '$rootScope', 'Auth', '$http', 'applyIcon', '$modal', '$log',
+        /**
+         * stack management controller
+         */
+        function($q, $location, $scope, $global, $rootScope, Auth, $http, applyIcon, $modal, $log) {
             // TODO: 
             Auth.stacks()
                 .then( function(stacks) {
 
                     $scope.stacks = stacks.data;
+                } );
+
+            Auth.instances()
+                .then( function(instances) {
+                    $scope.instances = instances.data;
                 } );
 
             $scope.applyIconClass = applyIcon;
@@ -18,6 +27,50 @@ angular.module( 'theme.pages-controllers' ).controller( 'stackManagementControll
                         //alert( JSON.stringify( stack.data ) );
                         $scope.stacks.push( stack.data );
                     } );
+            };
+
+            $scope.modify = function(stack) {
+                var modalInstance = $modal.open( {
+                    templateUrl: 'stackModification.html',
+                    controller: function($scope, $modalInstance, instances) {
+                        $scope.stack = stack;
+                        $scope.instances = instances;
+
+                        $scope.selected = {
+                            left_instances: [],
+                            right_instances: []
+                        };
+
+                        $scope.ok = function() {
+                            $modalInstance.close();
+                        };
+
+                        $scope.cancel = function() {
+                            $modalInstance.dismiss( 'cancel' );
+                        };
+
+                        $scope.add = function() {
+                            console.log( $scope.selected.left_instances );
+                        };
+                        $scope.remove = function() {
+                            console.log( $scope.selected.right_instances );
+                        };
+                    },
+                    resolve: {
+                        instances: function() {
+                            return $scope.instances;
+                        }
+                    }
+                } );
+
+                modalInstance.result.then( function() {}, function() {
+                    $log.info( 'Modal dismissed at: ' + new Date() );
+                } );
+            };
+
+            $scope.selectStack = function(stack, $index) {
+                $scope.selectedStack = stack;
+                $scope.selectedIndex = $index;
             };
         }
     ]
