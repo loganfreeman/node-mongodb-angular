161675ac398d08d1e3aa9205f7d6dd4569f924ca
schengcrouchingtiger <scheng@contactpointsolutions.com>
2015-04-16 16:51:57 -0600

    all

--- server/test/mongoose/instance.js
+++ server/test/mongoose/instance.js
@@ -1,142 +1,164 @@
-var _ = require('lodash');
+var _ = require( 'lodash' );
 
-var expect = require('chai').expect,
-    should = require('chai').should();
+var expect = require( 'chai' ).expect,
+    should = require( 'chai' ).should();
 
-var mongoose = require('mongoose');
+var mongoose = require( 'mongoose' );
 
 
-require('../../src/mongoose/models');
+require( '../../src/mongoose/models' );
 
-var Promise = require('bluebird');
+var Promise = require( 'bluebird' );
 
-var utils = require('../../src/resources/utils.js');
+var utils = require( '../../src/resources/utils.js' );
 
 function connect(database) {
-    return mongoose.createConnection('mongodb://localhost/' + database);
+    return mongoose.createConnection( 'mongodb://localhost/' + database );
 }
 
 
-describe('#instance#', function() {
+describe( '#instance#', function() {
     var db, Instance, Stack;
 
     var stacks = [];
 
-    before(function(done) {
+    before( function(done) {
         // add dummy data
-        db = connect('test');
-        Stack = db.model('Stack');
-        Instance = db.model('Instance');
+        db = connect( 'test' );
+        Stack = db.model( 'Stack' );
+        Instance = db.model( 'Instance' );
 
 
-        var stackPromise = Stack.create({
+        var stackPromise = Stack.create( {
             name: 'stack #1'
-        });
+        } );
 
-        var stackPromise1 = Stack.create({
+        var stackPromise1 = Stack.create( {
             name: 'stack #2'
-        });
+        } );
 
-        var stackPromise2 = Stack.create({
+        var stackPromise2 = Stack.create( {
             name: 'stack #3'
-        });
+        } );
 
-        var stackPromise3 = Stack.create({
+        var stackPromise3 = Stack.create( {
             name: 'stack #4'
-        });
+        } );
 
-        var instanceP1 = Instance.create({
+        var instanceP1 = Instance.create( {
             name: 'a',
             serviceType: 'PCP'
-        });
-        var instanceP2 = Instance.create({
+        } );
+        var instanceP2 = Instance.create( {
             name: 'b',
             serviceType: 'PCP'
-        });
+        } );
 
         var envId, stack;
 
-        Promise.all([stackPromise, stackPromise1, stackPromise2, stackPromise3, instanceP1, instanceP2])
-            .then(function(values) {
+        Promise.all( [stackPromise, stackPromise1, stackPromise2, stackPromise3, instanceP1, instanceP2] )
+            .then( function(values) {
 
-                stacks.push(values.shift());
-                stacks.push(values.shift());
-                stacks.push(values.shift());
-                stacks.push(values.shift());
+                stacks.push( values.shift() );
+                stacks.push( values.shift() );
+                stacks.push( values.shift() );
+                stacks.push( values.shift() );
                 done();
-            });
+            } );
 
-    });
+    } );
 
-    after(function(done) {
+    after( function(done) {
         // clean up the test db
-        db.db.dropDatabase(function() {
+        db.db.dropDatabase( function() {
             db.close();
             done();
-        });
-    });
+        } );
+    } );
 
-    it('should create instance', function(done) {
+    it( 'should create instance', function(done) {
         var instance;
-        var thenable = Instance.find({
-                name: 'a'
-            }).exec().then(function(instances) {
+        var thenable = Promise.resolve( Instance.find( {
+            name: 'a'
+        } ).exec() )
+            .then( function(instances) {
                 instance = instances.shift();
                 //console.log( instance );
-                instance.serviceType.should.be.eq('PCP');
+                instance.serviceType.should.be.eq( 'PCP' );
 
-                instance.stacks.push(stacks[0]._id);
+                instance.stacks.push( stacks[0]._id );
 
-                instance.stacks.push(stacks[1]._id);
-
-
-                return instance.save();
-            })
-            .then(function(instance) {
-                console.log(instance);
-                return Stack.find({
-                    _id: {
-                        $in: instance.stacks
-                    }
-                }).exec();
-            })
-            .then(function(models) {
-                console.log(models);
-
-                _.each(models, function(stack) {
-                    utils.exists(stack.instances, instance._id).should.be.eq(true);
-                })
-                instance.stacks.push(stacks[2]._id);
-
-                instance.stacks.push(stacks[3]._id);
+                instance.stacks.push( stacks[1]._id );
 
 
                 return instance.save();
-            })
-            .then(function(instance) {
-                console.log(instance);
-                return Stack.find({
+            } )
+            .then( function(instance) {
+                console.log( instance );
+                return Stack.find( {
                     _id: {
                         $in: instance.stacks
                     }
-                }).exec();
-            })
-            .then(function(stacks) {
-                console.log(stacks);
-
-                _.each(stacks, function(stack) {
-                    utils.exists(stack.instances, instance._id).should.be.eq(true);
-                })
-            });
-
-        Promise.resolve(thenable).then(function() {
-                done();
-            })
-            .catch(function(err) {
-                done(err);
-            })
-    });
+                } ).exec();
+            } )
+            .then( function(models) {
+                console.log( models );
 
 
+                var promises = [];
 
-});
+                promises.push( instance.update( {
+                    $addToSet: {
+                        stacks: {
+                            $each: [stacks[2], stacks[3]]
+                        }
+                    }
+                } ) );
+
+                promises.push( stacks[2].update( {
+                    $addToSet: {
+                        instances: instance
+                    }
+                } ) );
+
+                promises.push( stacks[3].update( {
+                    $addToSet: {
+                        instances: instance
+                    }
+                } ) );
+
+
+                return Promise.all( promises );
+            } )
+            .then( function(results) {
+                return Instance.find( {
+                    name: 'a'
+                } ).exec();
+            } )
+            .then( function(instance) {
+                console.log( instance );
+                return Stack.find( {
+                    _id: {
+                        $in: instance.stacks
+                    }
+                } ).exec();
+            } )
+            .then( function(stacks) {
+                console.log( stacks );
+
+                _.each( stacks, function(stack) {
+                    utils.exists( stack.instances, instance._id ).should.be.eq( true );
+                } );
+            } );
+
+        Promise.resolve( thenable ).then( function() {
+            done();
+        } )
+            .catch( function(err) {
+                done( err );
+            } );
+    } );
+
+
+
+} );
