3feab3eb8a360d25ddf67e4b1826aa67377a048f
schengcrouchingtiger <scheng@contactpointsolutions.com>
2015-04-21 11:44:15 -0600

    all

--- internal/app/scripts/pages/echoController.js
+++ internal/app/scripts/pages/echoController.js
@@ -1,7 +1,7 @@
 'use strict';
 angular
-    .module( 'theme.echo-controllers', [] )
-    .controller( 'echoController', function($scope) {
+    .module( 'theme.echo-controllers', ['rx'] )
+    .controller( 'echoController', function($scope, rx, $http) {
         $scope.reply = '';
         var primus = new Primus();
         primus.on( 'data', function received(data) {
@@ -12,4 +12,36 @@ angular
             primus.write( $scope.message );
             $scope.message = '';
         };
+
+        //Reactive Angular
+        function searchWikipedia(term) {
+            return rx.Observable
+                .fromPromise( $http( {
+                    url: 'http://en.wikipedia.org/w/api.php?&callback=JSON_CALLBACK',
+                    method: 'jsonp',
+                    params: {
+                        action: 'opensearch',
+                        search: term,
+                        format: 'json'
+                    }
+                } ) )
+                .map( function(response) {
+                    return response.data[1];
+                } );
+        }
+
+        $scope.search = '';
+        $scope.results = [];
+
+        /*
+          Creates a "click" function which is an observable sequence instead of just a function.
+        */
+        $scope.$createObservableFunction( 'click' )
+            .map( function() {
+                return $scope.search;
+            } )
+            .flatMapLatest( searchWikipedia )
+            .subscribe( function(results) {
+                $scope.results = results;
+            } );
     } );
